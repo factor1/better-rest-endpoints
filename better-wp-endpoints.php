2d071e4912e801b9bf557166d0f87a2b306c8590
Eric Stout <ericwstout@gmail.com>
2017-09-20 21:03:29 -0700

    Initial commit of plugin file

---
+++ better-wp-endpoints.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Better WordPress Endpoints
+Plugin URI:   https://github.com/erwstout/better-wp-endpoints/
+Description:  Serves up slimmer WordPress Rest API endpoints.
+Version:      0.0.1
+Author:       Eric Stout
+Author URI:   https://ericwstout.com/
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  better-wp-endpoints
+Domain Path:  /languages
+*/
