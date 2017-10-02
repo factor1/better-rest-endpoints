2bdd7c0647ae837ac7915fbb59e7ba2e1adc3a70
Eric Stout <ericwstout@gmail.com>
2017-10-02 15:29:06 -0700

    Update plugin information

--- better-wp-endpoints.php
+++ better-wp-endpoints.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:  Better WordPress Endpoints
-Plugin URI:   https://github.com/erwstout/better-wp-endpoints/
+Plugin URI:   https://github.com/factor1/better-wp-endpoints/
 Description:  Serves up slimmer WordPress Rest API endpoints.
 Version:      0.0.1
-Author:       Eric Stout
-Author URI:   https://ericwstout.com/
+Author:       Eric Stout, Factor1 Studios
+Author URI:   https://factor1studios.com/
 License:      GPL3
 License URI:  https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:  better-wp-endpoints
