3c795ef62256f7eb2d0818a612db4b2d288f758c
Eric Stout <ericwstout@gmail.com>
2018-01-11 16:25:09 -0700

    Rename plugin instances

--- better-wp-endpoints.php
+++ better-wp-endpoints.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name:  Better WordPress Endpoints
+Plugin Name:  Better Rest Endpoints
 Plugin URI:   https://github.com/factor1/better-wp-endpoints/
 Description:  Serves up slimmer WordPress Rest API endpoints, with some great enhancements.
 Version:      0.2.0
@@ -11,18 +11,18 @@ License URI:  https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:  better-wp-endpoints
 Domain Path:  /languages
 
-Better WordPress Endpoints is free software: you can redistribute it and/or modify
+Better Rest Endpoints is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
 
-Better WordPress Endpoints is distributed in the hope that it will be useful,
+Better Rest Endpoints is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Better WordPress Endpoints. If not, see {URI to Plugin License}.
+along with Better Rest Endpoints. If not, see {URI to Plugin License}.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
